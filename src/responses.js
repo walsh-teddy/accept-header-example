932b8bc10658dfa99f796c706928020253c25e3d
Teddy Walsh <teddywalsh72@gmail.com>
2023-02-03 15:11:49 -0500

    I don't remember tbh

--- src/responses.js
+++ src/responses.js
@@ -1,4 +1,4 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
@@ -10,6 +10,29 @@ const respond = (request, response, content, type) => {
 
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
+};
+
+const getCats = (request, response) => {
+  const theCat = {
+    name: 'Nibbles',
+    age: 4,
+  };
+
+  const preferredAccept = request.headers.accept.split(',')[0];
+
+  if (preferredAccept === 'application/json') {
+    respond(request, response, JSON.stringify(theCat), preferredAccept);
+  }
+  else if (preferredAccept === 'text/xml') {
+    const dataToSend = `
+      <response>
+        <name>${theCat.name}</name>
+        <age>${theCat.age}</age>
+      </response>
+    `.trim();
+
+    respond(request, response, dataToSend, preferredAccept);
+  }
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -4,14 +4,22 @@ const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
-
+// Create a dictionary of the possible pages to search through easily
+const routes = {
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex, // Default
 };
 
 const onRequest = (request, response) => {
+  console.log(request.url);
 
+  // Find the proper callback function
+  const thePropperRoute = routes[request.url] || routes.index;
+
+  thePropperRoute(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
